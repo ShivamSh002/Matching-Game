dd1cd915f8bc7c41afffe02aa73c5544462441e1
ShivamSh002 <shivamsharma98879@gmail.com>
2024-05-10 18:47:00 +0530

    Intro Screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
 import React from "react";
 import "./App.css";
 import LandingPage from "./Components/LandingPage/LandingPage";
+import { Routes, Route } from "react-router-dom";
+import Intro from "./Components/Intro/Intro";
+import Question from "./Components/Question/Question";
+import Instruction from "./Components/Instruction/Instruction";
 
 const App = () => {
   return (
-    <div className="wrapper">
-      <LandingPage />
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div className="wrapper">
+            <LandingPage />
+          </div>
+        }
+      />
+      <Route
+        path="/intro"
+        element={
+          <div className="wrapper">
+            <Intro />
+          </div>
+        }
+      />
+      <Route
+        path="/question"
+        element={
+          <div className="wrapper">
+            <Question />
+          </div>
+        }
+      />
+      <Route
+        path="/instruction"
+        element={
+          <div className="wrapper">
+            <Instruction />
+          </div>
+        }
+      />
+    </Routes>
   );
 };
 
--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,28 +1,39 @@
 import React from "react";
+import BackBtn from "../../assests/back.png";
+import styles from "./Intro.module.css";
 import Monkey from "../../assests/monkey.png";
-import styles from "./LandingPage.module.css";
-import StartBtn from "../../assests/startbtn.png";
+import NextBtn from "../../assests/next.png";
 import Cloud from "../../assests/cloud.png";
 import Star from "../../assests/Star.png";
 import Setting from "../../assests/setting.png";
+import banana from "../../assests/banana.png";
 import { useNavigate } from "react-router-dom";
 
-const LandingPage = () => {
+const Intro = () => {
   const nav = useNavigate();
   const handleClick = () => {
-    nav("/intro");
+    nav("/");
+  };
+  const handleNav = () => {
+    nav("/question");
   };
   return (
     <div>
+      <div className={styles.back}>
+        <img src={BackBtn} alt="" width={130} onClick={handleClick} />
+      </div>
       <div className={styles.wraaper}>
         <img src={Monkey} alt="monkey" width={550} className="monkey" />
       </div>
       <div className={styles.btn}>
-        <img src={StartBtn} alt="btn" width={300} onClick={handleClick} />
+        <img src={NextBtn} alt="btn" width={300} onClick={handleNav} />
       </div>
       <div className={styles.img}>
         <img src={Cloud} alt="cloud" width={450} />
-        <p>Welcome Kiddo !</p>
+        <p className={styles.intro}>
+          Hi , I am Mizo ! <br /> and I love bananas{" "}
+          <img src={banana} alt="" width={60} />
+        </p>
       </div>
       <div className={styles.star}>
         <img src={Star} alt="star" />
@@ -34,4 +45,4 @@ const LandingPage = () => {
   );
 };
 
-export default LandingPage;
+export default Intro;
